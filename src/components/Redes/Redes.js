5c5daaca6587d24fa951838575e709066255e3cb
Jeffer-UAO <jeffer443@gmail.com>
2023-06-30 05:41:52 -0500

    Version 5.0.1 Carrito de pedido

--- src/components/Redes/Redes.js
+++ src/components/Redes/Redes.js
@@ -4,6 +4,7 @@ import { IoLogoWhatsapp } from "react-icons/io";
 import { GiRotaryPhone } from "react-icons/gi";
 import { AiFillInstagram } from "react-icons/ai";
 import { BsFacebook } from "react-icons/bs";
+import { BsShop } from "react-icons/bs";
 
 
 
@@ -16,15 +17,11 @@ import { BsFacebook } from "react-icons/bs";
 export function Redes() {
   return <div className={styles.space}>   
     <div className={styles.phone}>
-      <p><IoLogoWhatsapp  size="20" color="#63f945" /> 322 663 0481 - 313 600 8914</p>
-      <p><GiRotaryPhone color="#ff0202" size="22"/> 881 6111</p>
+      <p><IoLogoWhatsapp  size="22" color="#63f945" /> 310 390 0666 - 312 752 7596 - 314 617 3076</p>
+      <p><GiRotaryPhone color="#ff0202" size="24"/> 881 6111 <BsShop color="#e5e619" size="22" /> Calle 15 # 9-64 Centro</p>
     </div>
 
   
-    <div className={styles.redes}>
-      <p>majos01 <AiFillInstagram size="22" color="orange" /> </p>
-      <p>@majos <BsFacebook color="blue" size="20"/></p>
    
-    </div>
   </div>;
 }
